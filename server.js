3cc1a6d81ec987c5c378e69efc3489ab47e07d73
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2017-03-25 04:17:43 +0000

    whoami impl

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var useragent = require('useragent');
 
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello ! Who am I"); 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', function (req, res) {
+    
+    var agent = useragent.parse(req.headers['user-agent']);
+    
+    res.json({
+        ipaddress: req.headers["x-forwarded-for"] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress,
+        language: req.headers["accept-language"].split(",")[0],
+        software:req.headers['user-agent'].split(') ')[0].split(' (')[1]
+    });
 });
 
 app.listen(process.env.PORT || 8080, function(){
